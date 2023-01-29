d214d4d18baea5877cf707551dfc72f1d4dc2ea5
Whyukim <devhyukim@gmail.com>
2023-01-29 12:00:00 +0900

    update inter

---
+++ 찰흙.js
@@ -0,0 +1,70 @@
+const canvas = document.querySelector("canvas");
+
+const ctx = canvas.getContext("2d");
+const dpr = window.devicePixelRatio;
+
+const canvasWidth = innerWidth;
+const canvasHeight = innerHeight;
+
+canvas.style.width = canvasWidth + "px";
+canvas.style.height = canvasHeight + "px";
+
+canvas.width = canvasWidth * dpr;
+canvas.height = canvasHeight * dpr;
+ctx.scale(dpr, dpr);
+
+class Particle {
+  constructor(x, y, radius, vy) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.vy = vy;
+  }
+
+  update() {
+    this.y += this.vy;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "#9ee3ee";
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+const particles = [];
+const TOTAL = 5;
+const randomNumBetween = (min, max) => Math.random() * (max - min + 1) + min;
+
+for (let i = 0; i < TOTAL; i++) {
+  const x = randomNumBetween(0, canvasWidth);
+  const y = randomNumBetween(0, canvasHeight);
+  const radius = randomNumBetween(50, 100);
+  const vy = randomNumBetween(1, 10);
+  const particle = new Particle(x, y, radius, vy);
+  particles.push(particle);
+}
+
+// fps
+let interval = 1000 / 60;
+let now, delta;
+let then = Date.now();
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  now = Date.now();
+  delta = now - then;
+
+  if (delta < interval) return;
+
+  particles.forEach((particle) => {
+    particle.update();
+    particle.draw();
+  });
+
+  then = now - (delta % interval);
+}
+
+animate();
